b9556200fb3c8e0389eba4e3b2e77f06234a437f
Maxwell Brown <maxwellgbrown@gmail.com>
2023-05-22 21:44:51 -0400

    Add simple pyramid wsgi server

---
+++ README.md
@@ -0,0 +1,19 @@
+# trolly
+
+A simple WSGI server backing a Zapier integration to emulate an event bus.
+
+# Development Quickstart
+
+0. Setup local environment
+
+   ```
+   python3 -m venv --prompt trolly env
+   source env/bin/activate
+   pip install -r requirements.txt
+   ```
+
+1. Run the local server
+
+   ```
+   python trolly.py
+   ```
---
+++ trolly.py
@@ -0,0 +1,30 @@
+"""Simple WSGI server backing a Zapier integration to emulate an event bus."""
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+
+
+class ConfigBuilder:
+    """Builder class to construct a pyramid.Configurator."""
+
+    def __init__(self):
+        self.config = Configurator()
+
+    def __call__(self):
+        self.config.add_route('hello', '/')
+        self.config.add_view(lambda r: Response("Hello world"), route_name='hello')
+
+        return self.config.make_wsgi_app()
+
+
+def main():
+    """Do the thing."""
+    config_builder = ConfigBuilder()
+    app = config_builder()
+
+    server = make_server('0.0.0.0', 6543, app)
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
